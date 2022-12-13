4b613e0ac727f50b603cff2610c0100943bf15f7
Priyan Rai <raipy@mail.uc.edu>
2022-12-13 12:46:07 -0500

    Initial Commit

---
+++ WorkJelly/WorkJellyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkJellyApp.swift
+//  WorkJelly
+//
+//  Created by Priyan Rai on 12/13/22.
+//
+
+import SwiftUI
+
+@main
+struct WorkJellyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
